a7560986e26d04dc5a87edef8b0076e4cab46795
Vinay Krishna Bukka <vinaykrishnabukka@gmail.com>
2023-12-12 14:18:19 -0500

    Modified test cpp for code coverage

--- test/test.cpp
+++ test/test.cpp
@@ -23,53 +23,53 @@
  * This test fixture includes two test cases for testing publisher and
  * subscriber functionalities using ROS 2 nodes.
  */
-// class TestNode : public testing::TestWithParam<int> {
-//   // Your test fixture code here
-//  public:
-//   rclcpp::Node::SharedPtr node1_;
-//   rclcpp::Node::SharedPtr node2_;
-// };
+class TestNode : public testing::TestWithParam<int> {
+  // Your test fixture code here
+ public:
+  rclcpp::Node::SharedPtr node1_;
+  rclcpp::Node::SharedPtr node2_;
+};
 
-// /**
-//  * @brief Test case for testing the publisher functionality.
-//  *
-//  * This test creates a ROS 2 node, creates a publisher, checks the number of
-//  * publishers on a topic, and asserts that the number matches the expected
-//  * value.
-//  */
-// TEST_F(TestNode, test_for_publisher) {
-//   node1_ = std::make_shared<rclcpp::Node>("test_publisher");
-//   // auto test_publisher =
-//   //    node1_->create_publisher<std_msgs::msg::String>("topic", 10);
-//   auto publishers_number = node1_->count_publishers("topic");
-//   EXPECT_EQ(3, static_cast<int>(publishers_number));
-// }
-
-// /**
-//  * @brief Test case for testing the subscriber functionality.
-//  *
-//  * This test creates a second ROS 2 node, subscribes to a topic, checks if the
-//  * received message is not null, and waits for a moment to allow the
-//  * subscription to be established.
-//  */
-// TEST_F(TestNode, test_for_subscriber) {
-//   node2_ = std::make_shared<rclcpp::Node>("test_subscriber");
-
-//   // Subscribe to the topic with a callback function
-//   auto subscription = node2_->create_subscription<std_msgs::msg::String>(
-//       "topic", 10, [this](const std_msgs::msg::String::SharedPtr msg) {
-//         // Check if the received message is not null
-//         ASSERT_NE(nullptr, msg);
-//       });
-
-//   // Wait for a moment to allow the subscription to be established
-//   rclcpp::sleep_for(std::chrono::seconds(1));
-// }
-
-
-TEST(dummy_test, this_should_pass) {
-  EXPECT_EQ(1, 1);
+/**
+ * @brief Test case for testing the publisher functionality.
+ *
+ * This test creates a ROS 2 node, creates a publisher, checks the number of
+ * publishers on a topic, and asserts that the number matches the expected
+ * value.
+ */
+TEST_F(TestNode, test_for_publisher) {
+  node1_ = std::make_shared<rclcpp::Node>("test_publisher");
+  // auto test_publisher =
+  //    node1_->create_publisher<std_msgs::msg::String>("topic", 10);
+  auto publishers_number = node1_->count_publishers("topic");
+  EXPECT_EQ(3, static_cast<int>(publishers_number));
 }
+
+/**
+ * @brief Test case for testing the subscriber functionality.
+ *
+ * This test creates a second ROS 2 node, subscribes to a topic, checks if the
+ * received message is not null, and waits for a moment to allow the
+ * subscription to be established.
+ */
+TEST_F(TestNode, test_for_subscriber) {
+  node2_ = std::make_shared<rclcpp::Node>("test_subscriber");
+
+  // Subscribe to the topic with a callback function
+  auto subscription = node2_->create_subscription<std_msgs::msg::String>(
+      "topic", 10, [this](const std_msgs::msg::String::SharedPtr msg) {
+        // Check if the received message is not null
+        ASSERT_NE(nullptr, msg);
+      });
+
+  // Wait for a moment to allow the subscription to be established
+  rclcpp::sleep_for(std::chrono::seconds(1));
+}
+
+
+// TEST(dummy_test, this_should_pass) {
+//   EXPECT_EQ(1, 1);
+// }
 
 
 /**
