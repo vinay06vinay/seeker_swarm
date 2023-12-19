c073bf9f753e43f5a63c46893c60fa86bca36270
NehaMadhekar09 <mneha1997@gmail.com>
2023-12-19 14:23:09 -0500

    Task 3.19: Added test cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,87 +1,131 @@
-/*
- * @file test.cpp
- * @brief This file contains the implementation of unit tests
- * @author Neha Nitin Madhekar,Vinay Krishna Bukka, Rashmi Kapu
- * @date 2023
- * @copyright Open Source Robotics Foundation, Inc.
- * @license Apache License, Version 2.0
- *    (you may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0)
- *
- */
 #include <gtest/gtest.h>
-#include <stdlib.h>
+#include "../src/node_robot_swarm.cpp"  
 
-#include <rclcpp/executor.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
-
-/**
- * @class TestNode
- * @brief Test fixture for testing ROS 2 node functionalities.
- *
- * This test fixture includes two test cases for testing publisher and
- * subscriber functionalities using ROS 2 nodes.
- */
-class TestNode : public testing::TestWithParam<int> {
-  // Your test fixture code here
- public:
-  rclcpp::Node::SharedPtr node1_;
-  rclcpp::Node::SharedPtr node2_;
+class RobotTest : public testing::Test {
+ protected:
+  std::shared_ptr<Robot> robot;
+  // std::shared_ptr<Master> master;
 };
 
-/**
- * @brief Test case for testing the publisher functionality.
- *
- * This test creates a ROS 2 node, creates a publisher, checks the number of
- * publishers on a topic, and asserts that the number matches the expected
- * value.
- */
-TEST_F(TestNode, test_for_publisher) {
-  node1_ = std::make_shared<rclcpp::Node>("test_publisher");
-  // auto test_publisher =
-  //    node1_->create_publisher<std_msgs::msg::String>("topic", 10);
-  auto publishers_number = node1_->count_publishers("topic");
-  EXPECT_EQ(3, static_cast<int>(publishers_number));
-}
-
-/**
- * @brief Test case for testing the subscriber functionality.
- *
- * This test creates a second ROS 2 node, subscribes to a topic, checks if the
- * received message is not null, and waits for a moment to allow the
- * subscription to be established.
- */
-TEST_F(TestNode, test_for_subscriber) {
-  node2_ = std::make_shared<rclcpp::Node>("test_subscriber");
-
-  // Subscribe to the topic with a callback function
-  auto subscription = node2_->create_subscription<std_msgs::msg::String>(
-      "topic", 10, [this](const std_msgs::msg::String::SharedPtr msg) {
-        // Check if the received message is not null
-        ASSERT_NE(nullptr, msg);
-      });
-
-  // Wait for a moment to allow the subscription to be established
-  rclcpp::sleep_for(std::chrono::seconds(1));
-}
-
-
-// TEST(dummy_test, this_should_pass) {
+///////////////////////////////////////////////////////////
+/// Tests
+///////////////////////////////////////////////////////////
+// TEST_F(RobotTest, robot_initialization) {
+//   auto r_namespace = "robot_" + std::to_string(1);
+//   auto nodename = "robot_" + std::to_string(1) + "_controller";
+//   robot = std::make_shared<Robot>(nodename, r_namespace);
+//   robot->set_goal(5.0, 5.0);
+//   robot->go_to_goal_callback();
 //   EXPECT_EQ(1, 1);
 // }
 
 
-/**
- * @brief Main function to run the ROS 2 node tests.
- *
- * Initializes ROS 2, runs all the tests, and shuts down ROS 2 on exit.
- *
- * @param argc Number of command-line arguments.
- * @param argv Array of command-line arguments.
- * @return Exit code.
- */
-int main(int argc, char** argv) {
+// Testing Number of Slaves Spawned Counting the publihsers
+TEST_F(RobotTest, slave_spawn_testing_publishers) {
+  int nodes = 10;
+  int pub_count = 0;
+  rclcpp::executors::MultiThreadedExecutor exec;
+  std::vector<std::shared_ptr<Robot>> robot_array;
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto nodename = "robot_" + std::to_string(i) + "_controller";
+    auto robot = std::make_shared<Robot>(nodename, r_namespace);
+    exec.add_node(robot);
+    robot_array.push_back(robot);
+  }
+
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto number_of_publishers =
+        robot_array[i]->count_publishers("/" + r_namespace + "/cmd_vel");
+    pub_count = pub_count + static_cast<int>(number_of_publishers);
+  }
+  EXPECT_EQ(nodes, pub_count);
+}
+
+// // Testing Number of Slaves Spawned_Counting the subscribers
+// TEST_F(RobotTest, slave_spawn_testing_subscribers) {
+//   int nodes = 10;
+//   int pub_count = 0;
+//   rclcpp::executors::MultiThreadedExecutor exec;
+//   std::vector<std::shared_ptr<Robot>> robot_array;
+//   for (int i = 0; i < nodes; i++) {
+//     auto r_namespace = "robot_" + std::to_string(i);
+//     auto nodename = "robot_" + std::to_string(i) + "_controller";
+//     auto robot = std::make_shared<Robot>(nodename, r_namespace);
+//     exec.add_node(robot);
+//     robot_array.push_back(robot);
+//   }
+//   for (int i = 0; i < nodes; i++) {
+//     auto r_namespace = "robot_" + std::to_string(i);
+//     auto number_of_subs =
+//         robot_array[i]->count_subscribers("/" + r_namespace + "/odom");
+//     pub_count = pub_count + static_cast<int>(number_of_subs);
+//   }
+//   EXPECT_EQ(nodes, pub_count);
+// }
+
+// Check Method Compute Distance
+TEST_F(RobotTest, compute_distance) {
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  std::pair<double, double> goal{10.0, 10.0};
+  std::pair<double, double> loc{0.0, 0.0};
+  double distance = robot->compute_distance(loc, goal);
+  double ex = 14.1421;
+  EXPECT_NEAR(ex, distance, 0.1);
+}
+
+// Check Method Nomrmalize Angle
+TEST_F(RobotTest, normalize_angle) {
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  double angle = robot->normalize_angle(-10.14);
+  double ex = 2.42637;
+  EXPECT_NEAR(ex, angle, 0.1);
+}
+
+// Check Method Nomrmalize Angle Positive
+TEST_F(RobotTest, test_normalize_angle_positive) {
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  double angle = robot->normalize_angle_positive(-7.29);
+  double ex = 5.2763;
+  EXPECT_NEAR(ex, angle, 0.1);
+}
+
+// Check Method Yaw From Quaternions
+TEST_F(RobotTest, compute_yaw_from_quaternion) {
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  robot->m_orientation.x = 3.0;
+  robot->m_orientation.y = 5.0;
+  robot->m_orientation.z = 4.0;
+  robot->m_orientation.w = 6.0;
+  double yaw = robot->compute_yaw_from_quaternion();
+  double ex = 1.51955;
+  EXPECT_NEAR(ex, yaw, 0.1);
+}
+
+TEST_F(RobotTest, num_publishers) {
+  auto r_namespace = "robot_" + std::to_string(1);
+  auto nodename = "robot_" + std::to_string(1) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+
+  auto number_of_publishers =
+      robot->count_publishers("/" + r_namespace + "/cmd_vel");
+  EXPECT_EQ(1, static_cast<int>(number_of_publishers));
+}
+
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   int result = RUN_ALL_TESTS();
