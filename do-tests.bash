a2f1d72c634b09e9dc6b5b57d4dba3ef7fe2219d
Vinay Krishna Bukka <vinaykrishnabukka@gmail.com>
2023-12-20 01:40:16 -0500

    Task 3.23 Setup Coverage Local

--- do-tests.bash
+++ do-tests.bash
@@ -34,34 +34,42 @@ colcon test-result --test-result-base build/seeker_swarm
 # 4. generate individual coverage reports:
 ##############################
 ## 4.1 my_model:
-colcon build \
-       --event-handlers console_cohesion+ \
-       --packages-select seeker_swarm \
-       --cmake-target "test_coverage" \
-       --cmake-arg -DUNIT_TEST_ALREADY_RAN=1
-MY_MODEL_COVERAGE_INFO=./build/seeker_swarm/test_coverage.info
+# colcon build \
+#        --event-handlers console_cohesion+ \
+#        --packages-select seeker_swarm \
+#        --cmake-target "test_coverage" \
+#        --cmake-arg -DUNIT_TEST_ALREADY_RAN=1
+# MY_MODEL_COVERAGE_INFO=./build/seeker_swarm/test_coverage.info
 ## 4.2 my_controller:
-#ros2 run my_controller generate_coverage_report.bash
-#MY_CONTROLLER_COVERAGE_INFO=./build/my_controller/test_coverage.info
+ros2 run seeker_swarm generate_coverage_report.bash
+MY_CONTROLLER_COVERAGE_INFO=./build/seeker_swarm/test_coverage.info
+MY_SEEKER_SWARM_COVERAGE_INFO = ./build/seeker_swarm/test_coverage
+open $MY_SEEKER_SWARM_COVERAGE_INFO/index.html || true
+
+
+# lcov -a $MY_MODEL_COVERAGE_INFO -a \
+#      $MY_CONTROLLER_COVERAGE_INFO -o \
+#      $ALL_COVERAGE_INFO
+
 
 ##############################
 # 5. Combine coverage reports
 ##############################
 ## create output directory
-COMBINED_TEST_COVERAGE=combined_test_coverage
-if [[ -d $COMBINED_TEST_COVERAGE ]]; then
-   rm -rf $COMBINED_TEST_COVERAGE
-fi
-mkdir $COMBINED_TEST_COVERAGE
-## combine the reports
-ALL_COVERAGE_INFO=./build/test_coverage_merged.info
-lcov -a $MY_MODEL_COVERAGE_INFO -a \
-     $MY_CONTROLLER_COVERAGE_INFO -o \
-     $ALL_COVERAGE_INFO
+# COMBINED_TEST_COVERAGE=combined_test_coverage
+# if [[ -d $COMBINED_TEST_COVERAGE ]]; then
+#    rm -rf $COMBINED_TEST_COVERAGE
+# fi
+# mkdir $COMBINED_TEST_COVERAGE
+# ## combine the reports
+# ALL_COVERAGE_INFO=./build/test_coverage_merged.info
+# lcov -a $MY_MODEL_COVERAGE_INFO -a \
+#      $MY_CONTROLLER_COVERAGE_INFO -o \
+#      $ALL_COVERAGE_INFO
 
-genhtml --output-dir $COMBINED_TEST_COVERAGE $ALL_COVERAGE_INFO
+# genhtml --output-dir $COMBINED_TEST_COVERAGE $ALL_COVERAGE_INFO
 
-##############################
-# 6. show the combined coverage report
-##############################
-open $COMBINED_TEST_COVERAGE/index.html || true
+# ##############################
+# # 6. show the combined coverage report
+# ##############################
+# open $COMBINED_TEST_COVERAGE/index.html || true
--- test/test.cpp
+++ test/test.cpp
@@ -11,29 +11,47 @@
  */
 #include <gtest/gtest.h>
 #include "../src/node_robot_swarm.cpp"  
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp> 
+#include <nav_msgs/msg/odometry.hpp>
 
 class RobotTest : public testing::Test {
  protected:
   std::shared_ptr<RobotSwarm> robot;
-  // std::shared_ptr<Master> master;
 };
-
-///////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 /// Tests
 ///////////////////////////////////////////////////////////
-// TEST_F(RobotTest, robot_initialization) {
-//   auto r_namespace = "robot_" + std::to_string(1);
-//   auto nodename = "robot_" + std::to_string(1) + "_controller";
-//   robot = std::make_shared<Robot>(nodename, r_namespace);
-//   robot->set_goal(5.0, 5.0);
-//   robot->go_to_goal_callback();
-//   EXPECT_EQ(1, 1);
-// }
+
+TEST_F(RobotTest, move_flag_test) {
+  auto r_namespace = "robot_" + std::to_string(1);
+  auto nodename = "robot_" + std::to_string(1) + "_controller";
+  robot = std::make_shared<RobotSwarm>(nodename, r_namespace);
+  std_msgs::msg::Bool::SharedPtr msg;
+  robot->move_flag_callback(msg);
+  EXPECT_EQ(2, 2);
+}
+
+TEST_F(RobotTest, stop_method) {
+  auto r_namespace = "robot_" + std::to_string(1);
+  auto nodename = "robot_" + std::to_string(1) + "_controller";
+  robot = std::make_shared<RobotSwarm>(nodename, r_namespace);
+  robot->stop();
+  EXPECT_EQ(2, 2);
+}
+TEST_F(RobotTest, robot_initialization) {
+  auto r_namespace = "robot_" + std::to_string(1);
+  auto nodename = "robot_" + std::to_string(1) + "_controller";
+  robot = std::make_shared<RobotSwarm>(nodename, r_namespace);
+  robot->set_goal(5.0, 5.0);
+  robot->navigate_callback();
+  EXPECT_EQ(1, 1);
+}
 
 
-// Testing Number of Slaves Spawned Counting the publihsers
-TEST_F(RobotTest, slave_spawn_testing_publishers) {
-  int nodes = 10;
+TEST_F(RobotTest, count_testing_publishers) {
+  int nodes = 5;
   int pub_count = 0;
   rclcpp::executors::MultiThreadedExecutor exec;
   std::vector<std::shared_ptr<RobotSwarm>> robot_array;
@@ -54,27 +72,27 @@ TEST_F(RobotTest, slave_spawn_testing_publishers) {
   EXPECT_EQ(nodes, pub_count);
 }
 
-// // Testing Number of Slaves Spawned_Counting the subscribers
-// TEST_F(RobotTest, slave_spawn_testing_subscribers) {
-//   int nodes = 10;
-//   int pub_count = 0;
-//   rclcpp::executors::MultiThreadedExecutor exec;
-//   std::vector<std::shared_ptr<Robot>> robot_array;
-//   for (int i = 0; i < nodes; i++) {
-//     auto r_namespace = "robot_" + std::to_string(i);
-//     auto nodename = "robot_" + std::to_string(i) + "_controller";
-//     auto robot = std::make_shared<Robot>(nodename, r_namespace);
-//     exec.add_node(robot);
-//     robot_array.push_back(robot);
-//   }
-//   for (int i = 0; i < nodes; i++) {
-//     auto r_namespace = "robot_" + std::to_string(i);
-//     auto number_of_subs =
-//         robot_array[i]->count_subscribers("/" + r_namespace + "/odom");
-//     pub_count = pub_count + static_cast<int>(number_of_subs);
-//   }
-//   EXPECT_EQ(nodes, pub_count);
-// }
+
+TEST_F(RobotTest, count_testing_subscribers) {
+  int nodes = 10;
+  int pub_count = 0;
+  rclcpp::executors::MultiThreadedExecutor exec;
+  std::vector<std::shared_ptr<RobotSwarm>> robot_array;
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto nodename = "robot_" + std::to_string(i) + "_controller";
+    auto robot = std::make_shared<RobotSwarm>(nodename, r_namespace);
+    exec.add_node(robot);
+    robot_array.push_back(robot);
+  }
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto number_of_subs =
+        robot_array[i]->count_subscribers("/" + r_namespace + "/odom");
+    pub_count = pub_count + static_cast<int>(number_of_subs);
+  }
+  EXPECT_EQ(nodes, pub_count);
+}
 
 // Check Method Compute Distance
 TEST_F(RobotTest, euclid_dist) {
@@ -136,10 +154,19 @@ TEST_F(RobotTest, num_publishers) {
   EXPECT_EQ(1, static_cast<int>(number_of_publishers));
 }
 
-int main(int argc, char **argv) {
+TEST_F(RobotTest, image_callback_test) {
+  auto r_namespace = "robot_" + std::to_string(1);
+  auto nodename = "robot_" + std::to_string(1) + "_controller";
+  robot = std::make_shared<RobotSwarm>(nodename, r_namespace);
+  sensor_msgs::msg::Image::SharedPtr test_image_msg = std::make_shared<sensor_msgs::msg::Image>();
+  robot->imageCallback(test_image_msg);
+  EXPECT_EQ(2.5, 2.5);
+}
+int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   int result = RUN_ALL_TESTS();
+  std::cout << "DONE SHUTTING DOWN" << std::endl;
   rclcpp::shutdown();
   return result;
 }
